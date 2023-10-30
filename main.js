3dcc4b0b5a3b3fc9f6617ec3b18ae8da94ba2284
RuslanVelizade <ruslanveli57@gmail.com>
2023-10-30 19:56:37 +0400

    first js task

---
+++ main.js
@@ -0,0 +1,43 @@
+// task 1
+// const firstName = "Ruslan";
+// const lastName = "Velizade";
+// const fullName = firstName + " " + lastName;
+// console.log(fullName);
+const fullName = "Jon Doe.";
+const country = "Azerbaijan.";
+const age = "20 ";
+const job = "teacher.";
+const personalInfo = "My name is " + fullName +  "I live in " + country + "I am a " + job + "I am  " + age + "years old.";
+console.log( personalInfo);
+// task 2
+let length = 16; 
+console.log(typeof length);
+let color = "Brown";
+let float = "12.5";
+let like = true;
+
+let x = {
+    firstName: " Nick",
+    color: "Red",
+};
+// task 3
+let distance = 8600;
+let speed = 120;
+let duration = distance/speed;
+console.log("The car will arrive in Chine in " + duration + " minutes");
+// task 4
+let born = 2003;
+let now  = 2023;
+let between = 2024
+let difference = now-born;
+let dif = between-born;
+console.log("I will be either " + difference + " or " + dif);
+// task 5
+let student1 = 30;
+let student2 = 35;
+let student3 = 40;
+let student4 = 45;
+let count = 4;
+let sum = student1 + student2 + student3 + student4;
+let division = sum/count;
+console.log("Telebelerin ortalamasi: " + division);
