9060fbc9d11c1852db512df7c53644285eb84806
RuslanVelizade <ruslanveli57@gmail.com>
2023-11-01 19:49:32 +0400

    new task

---
+++ 3.JS-loop/main.js
@@ -0,0 +1,48 @@
+// //TASK 
+//   for(let index = 0; index < 100; index++) {
+//     if (index % 2 === 0) console.log(index);
+//   }
+//   //TASK 2
+//   let userName = "Ruslan";
+  
+//   for (let index = 0; index < userName.length; index++) {
+//     console.log(userName[index]);
+//   }
+//   //TASK 3
+//   for (let index = 17; index <100; index = index + 10) {
+//     console.log(index);
+//   }
+//   //TASK 4
+//   for (let index = 11; index < 100; index = index + 11) {
+//     console.log(index);
+//   }
+//   //TASK 5
+//   let a = 40;
+//   for (let index = 0; index <=a ; index++) {
+//      if (a%index===0) {
+//       console.log(index);
+//      }
+//   }
+  
+//   //TASK 6
+//   let x = 1;
+//   for (let index = 1; index < 10; index++) {
+//     x=x*index
+//     console.log(index);
+//   }
+  
+//   //TASK 7
+//   // let x = 1;
+//   // for (let index = 1; index < 10; index++) {
+//   //     index=index+x
+//   //     console.log(index);
+//   // }
+// // }
+// //TASK 8
+// for (let index = 10; 10 <index < 50; index++) {
+//    if (index%3===0) {
+//     console.log(index);
+//    } 
+    
+// }
+
