0c16e67de50a61d4f6d0830b70a853d4c846c8e7
RuslanVelizade <ruslanveli57@gmail.com>
2023-11-02 19:44:00 +0400

    math task js

---
+++ 4.JS-Math/main.js
@@ -0,0 +1,12 @@
+let number  = 17.23;
+console.log(Math.floor(number)); // 17 
+console.log(Math.ceil(number));//18
+console.log(Math.round(number));//17
+console.log(Math.trunc(number));//17
+console.log(Math.sign(-7));//-1
+console.log(Math.pow(4,2));//16
+console.log(Math.sqrt(81));//9
+console.log(Math.abs(-9.7));//9.7
+console.log(Math.min(5,7,16,24));//5
+console.log(Math.max(67,8,9,34,80));//80
+console.log(Math.random());
