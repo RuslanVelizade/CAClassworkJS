410ee31b8e89a7888328c81bee2d55d0d1d8deba
RuslanVelizade <ruslanveli57@gmail.com>
2023-11-09 22:17:50 +0400

    new

--- 5.JS-Array & Object/main.js
+++ 5.JS-Array & Object/main.js
@@ -13,17 +13,17 @@ min = numbers[i];
 console.log(max);
 console.log(min);
 //TASK 2
-let num = [12, 45, 6, 78, 34, 99, 23];
-let addition = 0;
-for (let i = 0; i < num.length; i++) {
-    addition += num[i];
-}
-console.log(addition);
+// let num = [12, 45, 6, 78, 34, 99, 23];
+// let addition = 0;
+// for (let i = 0; i < num.length; i++) {
+//     addition += num[i];
+// }
+// console.log(addition);
 //TASK 3
-let elements = [1, 2, 3, 4, 5];
-// let order = elements;
-[elements[0], elements[elements.length-1]]=[elements[elements.length-1], elements[0]]
-console.log(elements);
+// let elements = [1, 2, 3, 4, 5];
+// // let order = elements;
+// [elements[0], elements[elements.length-1]]=[elements[elements.length-1], elements[0]]
+// console.log(elements);
 
 
 
