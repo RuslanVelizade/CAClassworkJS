5040c4a35051a951a7360c1b1cdc0fabb45e8201
RuslanVelizade <ruslanveli57@gmail.com>
2023-11-15 20:02:20 +0400

    object task

--- 9.JS-Array-and-string-methods/main.js
+++ 9.JS-Array-and-string-methods/main.js
@@ -49,11 +49,11 @@ isPalindrome("hello")
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 const length = 4;
 
-function splitIntoChunk(arr, len) {
+function splitIntoChunk(arr, length) {
     
     return arr;
 }
 
 
 
-console.log(splitIntoChunk(array, leng)); // [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ] ]
+console.log(splitIntoChunk(array, length)); // [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ] ]
