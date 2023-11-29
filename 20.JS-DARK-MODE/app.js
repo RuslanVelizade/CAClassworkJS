4d2f0f1b50c8626df3f7b5b03fd83d65f7bf020c
RuslanVelizade <ruslanveli57@gmail.com>
2023-11-29 18:38:51 +0400

    new task

---
+++ 20.JS-DARK-MODE/app.js
@@ -0,0 +1,21 @@
+let body = document.querySelector("body");
+let btn = document.querySelector(".btn");
+
+let isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+if (isDarkMode) {
+    body.classList.add('dark-mode')
+}
+btn.addEventListener("click", function () {
+    
+    body.classList.toggle("dark-mode");
+
+    if () {
+        
+    }
+    
+});
+
+
+
+
