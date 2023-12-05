e82c0de1a6cf6ace1e7b4019f1dfbae52b1294c4
RuslanVelizade <ruslanveli57@gmail.com>
2023-12-05 21:37:37 +0400

    wheather api

--- 24.1JS-weather-api/weather.js
+++ 24.1JS-weather-api/weather.js
@@ -1,10 +1,31 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const overCastDiv = document.querySelector(".overcast");
+
 
 function getWeather(curCity) {
     fetch(`https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${curCity}&aqi=no`)
         .then((response) => response.json())
-        .then((data) => console.log(data)
-            
-        )
+          .then((data) =>
+        //console.log(data) 
+        {
+            overCastDiv.innerHTML = `
+            <div>
+                <p>${data.location.name}, ${data.location.country}</p>
+                <img src="${data.current.condition.icon}" alt="">
+                <p>${data.current.condition.text}</p>
+                <p>Selsie: ${data.current.temp_c}</p>
+                <p>Farenheigth: ${data.current.temp_f}</p>
+                <p></p>
+            </div>
+            `
+        }
+           )   
     .catch((err)=>console.log(err))
 }
-getWeather("Ganja");
+getWeather();
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    getWeather(input.value);
+})
